bf9d404b4bac90044843d60e8fbb9eb445eacd8f
Brendan Matkin <brendan@brendanmatkin.info>
2014-07-02 19:53:45 -0600

    test scene to verify camera facing billboard script

---
+++ Assets/Scripts/Brendan/CameraFacingBillboard.cs
@@ -0,0 +1,54 @@
+// http://wiki.unity3d.com/index.php?title=CameraFacingBillboard
+
+/*using UnityEngine;
+using System.Collections;
+
+public class CameraFacingBillboard : MonoBehaviour
+{
+	public Camera m_Camera;
+	
+	void Update()
+	{
+		transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.back,
+		                 m_Camera.transform.rotation * Vector3.up);
+	}
+}*/
+
+//cameraFacingBillboard.cs v02
+//by Neil Carter (NCarter)
+//modified by Juan Castaneda (juanelo)
+//
+//added in-between GRP object to perform rotations on
+//added auto-find main camera
+//added un-initialized state, where script will do nothing
+using UnityEngine;
+using System.Collections;
+
+
+public class CameraFacingBillboard : MonoBehaviour
+{
+	
+	public Camera m_Camera;
+	public bool amActive =false;
+	public bool autoInit =false;
+	GameObject myContainer;	
+	
+	void Awake(){
+		if (autoInit == true){
+			m_Camera = Camera.main;
+			amActive = true;
+		}
+		
+		myContainer = new GameObject();
+		myContainer.name = "GRP_"+transform.gameObject.name;
+		myContainer.transform.position = transform.position;
+		transform.parent = myContainer.transform;
+	}
+	
+	
+	void Update(){
+		if(amActive==true){
+			myContainer.transform.LookAt(myContainer.transform.position + m_Camera.transform.rotation * Vector3.back, m_Camera.transform.rotation * Vector3.up);
+		}
+	}
+}
---
+++ Assets/Scripts/Brendan/Platformer2DUserControl_MOD.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlatformerCharacter2D_MOD))]
+public class Platformer2DUserControl_MOD : MonoBehaviour 
+{
+	private PlatformerCharacter2D_MOD character;
+    private bool jump;
+
+
+	void Awake()
+	{
+		character = GetComponent<PlatformerCharacter2D_MOD>();
+	}
+
+    void Update ()
+    {
+        // Read the jump input in Update so button presses aren't missed.
+#if CROSS_PLATFORM_INPUT
+        if (CrossPlatformInput.GetButtonDown("Jump")) jump = true;
+#else
+		if (Input.GetButtonDown("Jump")) jump = true;
+#endif
+
+    }
+
+	void FixedUpdate()
+	{
+		// Read the inputs.
+		bool crouch = Input.GetKey(KeyCode.LeftControl);
+		#if CROSS_PLATFORM_INPUT
+		float h = CrossPlatformInput.GetAxis("Horizontal");
+		float v = CrossPlatformInput.GetAxis ("Vertical");
+		#else
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+		#endif
+
+		// Pass all parameters to the character control script.
+		character.Move( v, h, crouch , jump );
+
+        // Reset the jump input once it has been used.
+	    jump = false;
+	}
+}
