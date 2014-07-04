d386696140dcf2bc98e336dc0a3d66148cc7a14f
Brendan Matkin <brendan@brendanmatkin.info>
2014-07-03 19:45:06 -0600

    update tintSprite to tint children. Grouped flowers under tinterator parent

--- Assets/Scripts/Brendan/TintSprite.cs
+++ Assets/Scripts/Brendan/TintSprite.cs
@@ -11,12 +11,18 @@ public class TintSprite : MonoBehaviour
 	public Color tintColor = new Color(0.0f,0.5f,1.0f,0.9f);
 
 	void Awake(){
-		transform.renderer.material.color = tintColor;
+		foreach (Transform child in transform) {
+            child.transform.renderer.material.color = tintColor;
+        }
+		//transform.renderer.material.color = tintColor;
 	}
 
 	void Update(){
 		if (continuousUpdate == true){
-			transform.renderer.material.color = tintColor;	
+			//transform.renderer.material.color = tintColor;	
+			foreach (Transform child in transform) {
+                child.transform.renderer.material.color = tintColor;
+            }
 		}
 	}
 }
