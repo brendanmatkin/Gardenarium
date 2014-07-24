29c1ce50fdd16aad4a0efddbf33cf671645380ae
Brendan Matkin <brendan@brendanmatkin.info>
2014-07-24 16:06:19 -0600

    particle toon bubble tests, new terrain, rotation tests, audio tests, others. Kind of a lot...

---
+++ Assets/Scripts/KL_rotate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KL_rotate : MonoBehaviour {
+	//public float speed;
+	public float rotation;	
+
+	void Update () {
+		//if (Input.GetKey (KeyCode.K)) {
+		//	rigidbody.AddForce(-Vector2.right * speed);  
+		//}
+		//if (Input.GetKey (KeyCode.L)) {
+		//	rigidbody.AddForce(Vector2.right * speed);
+		//}
+		transform.Rotate(Input.GetAxis("Mouse ScrollWheel") * rotation * Time.deltaTime, 0, 0, Space.World); //This is where you rotate your GameObject
+	}
+
+    	
+}
